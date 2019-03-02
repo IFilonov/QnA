0b4def349e426225aef1101e3936dbf29a54649d
IFilonov <filonovia@gmail.com>
2019-03-03 01:10:07 +0500

    Add specs for AnswerController new and create

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+  let(:answer) { create(:answer, question: question) }
+
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question }  }
+
+    it 'assigns a new Answer to @answer' do
+      expect(assigns(:answer)).to be_a_new(Answer)
+    end
+
+    it 'renders new view' do
+      expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves a new answer of question in the database' do
+        expect { post :create, params: { answer: attributes_for(:answer), question_id: question } }.to change(question.answers, :count).by(1)
+      end
+
+      it 'redirect to question view' do
+        post :create, params: { answer: attributes_for(:answer), question_id: question }
+        expect(response).to redirect_to question
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the answer' do
+        expect { post :create, params: { answer: attributes_for(:answer, :invalid_answer), question_id: question } }.to_not change(Answer, :count)
+      end
+
+      it 'render questions/show' do
+        post :create, params: { answer: attributes_for(:answer, :invalid_answer), question_id: question }
+        expect(response).to render_template 'questions/show'
+      end
+    end
+  end
+end
