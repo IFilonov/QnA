717bf30451408da0cefb3cedb892a9360e8885d1
IFilonov <filonovia@gmail.com>
2019-03-06 00:28:17 +0500

    Any user can create a question

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  ALERT_TYPES = {
+    alert: "danger",
+    notice: "info"
+  }.freeze
+
+  def flash_messages
+    flash.map do |type, message|
+      class_type = "alert alert-#{ALERT_TYPES[type.to_sym]}"
+      content_tag :div, message, class: class_type, role: 'alert'
+    end.join("\n").html_safe
+  end
 end
