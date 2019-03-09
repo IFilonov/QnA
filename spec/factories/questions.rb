bfc2beaeb3cf83d0e7712a12129b27114406fe31
IFilonov <filonovia@gmail.com>
2019-03-10 00:08:22 +0500

    Any user can see all questions

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,10 +1,18 @@
 FactoryBot.define do
+  sequence :title do |n|
+    "MyString #{n}"
+  end
+
+  sequence :body do |n|
+    "MyBody #{n}"
+  end
+
   factory :question do
-    title { "MyString" }
-    body { "MyText" }
+    title { generate :title }
+    body { generate :body }
   end
 
   trait :invalid do
     title { nil }
-  end  
+  end
 end
---
+++ spec/features/question/index_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User can see all questions', %q{
+  In order to choose interesting question
+  As a regular user
+  I'd like to see all questions
+} do
+  given!(:questions) { create_list(:question, 5) }
+
+  scenario 'see all questions' do
+    visit questions_path
+    expect(page).to have_content 'All questions'
+    questions.each do |question|
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body
+    end
+  end
+
+end
