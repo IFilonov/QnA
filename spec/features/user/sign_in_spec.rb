2ff245ad72c3def72387ac5a4e7f4a3d97996948
IFilonov <filonovia@gmail.com>
2019-03-11 15:54:07 +0500

    User can sign out

--- spec/features/user/sign_in_spec.rb
+++ spec/features/user/sign_in_spec.rb
@@ -6,24 +6,24 @@ require 'rails_helper'
   I'd like to be able to sign in
 } do
 
-   given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
+  given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
 
-   background { visit new_user_session_path }
+  background { visit new_user_session_path }
 
-   scenario 'Registered user tries to sign in' do
+  scenario 'Registered user tries to sign in' do
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
 
     click_on 'Log in'
 
-     expect(page).to have_content 'Signed in successfully.'
+    expect(page).to have_content 'Signed in successfully.'
   end
 
-   scenario 'Unregistered user tries to sign in' do
+  scenario 'Unregistered user tries to sign in' do
     fill_in 'Email', with: 'wrong@test.com'
     fill_in 'Password', with: '12345678'
     click_on 'Log in'
 
-     expect(page).to have_content 'Invalid Email or password.'
+    expect(page).to have_content 'Invalid Email or password.'
   end
 end
---
+++ spec/features/user/sign_out_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+ feature 'User can sign out', %q{
+  As an authenticated user
+  I'd like to be able to sign out
+} do
+
+  given(:user) { User.create!(email: 'user@test.com', password: '12345678') }
+
+  background { visit new_user_session_path }
+
+  scenario 'Registered user tries to sign in' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+
+    click_on 'Log in'
+    click_on 'Log out'
+    expect(page).to have_content 'Signed out successfully.'
+  end
+end
