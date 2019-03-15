91abf93751c7e32a457878780a03381c6138cf1a
IFilonov <filonovia@gmail.com>
2019-03-15 21:08:43 +0500

    be_author_of in model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,20 +7,20 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
 
-  describe '#author?' do
+  describe 'user author?' do
     let(:user) { create(:user) }
     let(:another_user) { create(:user) }
 
     it 'when question owner is user' do
       question = create(:question, user: user)
 
-      expect(user.author_of?(question)).to eq true
+      expect(user).to be_author_of(question)
     end
 
     it 'when question owner is an another user' do
       question = create(:question, user: another_user)
 
-      expect(user.author_of?(question)).to eq false
+      expect(user).not_to be_author_of(question)
     end
   end
 end
