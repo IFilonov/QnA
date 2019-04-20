f0c5fb2e31e87cb08082ea65758a925789d59f5d
IFilonov <filonovia@gmail.com>
2019-04-20 11:30:55 +0500

    Add one file in question

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe Question, type: :model do
 
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
-  it { should have_db_index :user_id }  
+  it { should have_db_index :user_id }
+
+  it 'it have one attached file' do
+    expect(Question.new.file).to be_an_instance_of(ActiveStorage::Attached::One)
+  end
+
 end
